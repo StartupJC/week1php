77c67342991a6b1946cf6ae55e73f169ff62fb0a
--replace-all <Serghar12@gmail.com>
2016-04-06 17:46:02 -0700

    Added minimal changes

--- basics/registration/success.php
+++ basics/registration/success.php
@@ -4,6 +4,7 @@
 
 </head>
 <body>
+<!-- SOLID!! -->
 <img src="http://mediafluency.journalism.wisc.edu/wp-content/uploads/2015/01/awesome.gif" alt="Everything is Awesome" style="width:100%;height:100%;">
 </body>
 </html>
